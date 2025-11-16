e23ab62a2dfdc597847ded44b7c83e30cc1f13f0
Ben Hornedo <ben@uptown4.com>
2025-11-16 09:31:15 -0500

    feat: update auth layout branding to "SAIVD Viewer"

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({children}: {children: ReactNode}) {
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="flex justify-center">
-            <div className="h-10 w-auto text-3xl font-bold">SAIVD</div>
+            <div className="h-10 w-auto text-3xl font-bold">SAIVD Viewer</div>
           </div>
         </div>
         {children}
